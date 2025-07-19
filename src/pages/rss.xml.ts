a9a7c0a6699cefefacf7f56c34f36db1107f302f
Jursin <1836059252@qq.com>
2025-07-19 11:53:03 +0800

    fix: 修复构建错误

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,83 +1,52 @@
-import type { AstroGlobal, ImageMetadata } from 'astro'
-import { getImage } from 'astro:assets'
-import type { CollectionEntry } from 'astro:content'
-import rss from '@astrojs/rss'
-import type { Root } from 'mdast'
-import rehypeStringify from 'rehype-stringify'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import { unified } from 'unified'
-import { visit } from 'unist-util-visit'
-import config from 'virtual:config'
+import type { AstroGlobal } from 'astro';
+import type { CollectionEntry } from 'astro:content';
+import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import config from 'virtual:config';
 
-import { getBlogCollection, sortMDByDate } from 'astro-pure/server'
-
-// Get dynamic import of images as a map collection
-const imagesGlob = import.meta.glob<{ default: ImageMetadata }>(
-  '/src/content/blog/**/*.{jpeg,jpg,png,gif,avif.webp}' // add more image formats if needed
-)
-
-const renderContent = async (post: CollectionEntry<'blog'>, site: URL) => {
-  // Replace image links with the correct path
-  function remarkReplaceImageLink() {
-    /**
-     * @param {Root} tree
-     */
-    return async function (tree: Root) {
-      const promises: Promise<void>[] = []
-      visit(tree, 'image', (node) => {
-        if (node.url.startsWith('/images')) {
-          node.url = `${site}${node.url.replace('/', '')}`
-        } else {
-          const imagePathPrefix = `/src/content/blog/${post.id}/${node.url.replace('./', '')}`
-          const promise = imagesGlob[imagePathPrefix]?.().then(async (res) => {
-            const imagePath = res?.default
-            if (imagePath) {
-              node.url = `${site}${(await getImage({ src: imagePath })).src.replace('/', '')}`
-            }
-          })
-          if (promise) promises.push(promise)
-        }
-      })
-      await Promise.all(promises)
-    }
-  }
-
+const renderContent = async (post: CollectionEntry<'apps'>) => {
   const file = await unified()
     .use(remarkParse)
-    .use(remarkReplaceImageLink)
     .use(remarkRehype)
     .use(rehypeStringify)
-    .process(post.body)
+    .process(post.body);
+  return String(file);
+};
 
-  return String(file)
-}
-
-const GET = async (context: AstroGlobal) => {
-  const allPostsByDate = sortMDByDate(await getBlogCollection()) as CollectionEntry<'blog'>[]
-  const siteUrl = context.site ?? new URL(import.meta.env.SITE)
+export const GET = async (context: AstroGlobal) => {
+  const allItems = await getCollection('apps');
+  
+  const allItemsByDate = allItems.sort((a, b) => 
+    new Date(b.data.publishDate ?? 0).getTime() - 
+    new Date(a.data.publishDate ?? 0).getTime()
+  );
 
   return rss({
-    // Basic configs
+    // 基础配置
+    title: config.title,
+    description: config.description,
+    site: context.site?.toString() ?? import.meta.env.SITE,
     trailingSlash: false,
     xmlns: { h: 'http://www.w3.org/TR/html4/' },
     stylesheet: '/scripts/pretty-feed-v3.xsl',
 
-    // Contents
-    title: config.title,
-    description: config.description,
-    site: import.meta.env.SITE,
+    // 内容项
     items: await Promise.all(
-      allPostsByDate.map(async (post) => ({
-        pubDate: post.data.publishDate,
-        link: `/blog/${post.id}`,
-        customData: `<h:img src="${typeof post.data.heroImage?.src === 'string' ? post.data.heroImage?.src : post.data.heroImage?.src.src}" />
-          <enclosure url="${typeof post.data.heroImage?.src === 'string' ? post.data.heroImage?.src : post.data.heroImage?.src.src}" />`,
-        content: await renderContent(post, siteUrl),
-        ...post.data
+      allItemsByDate.map(async (item) => ({
+        title: item.data.title,
+        description: item.data.description ?? '',
+        pubDate: item.data.publishDate ?? new Date(),
+        link: `/apps/${item.id}/`,
+        content: await renderContent(item),
+        customData: `
+          ${item.data.tags?.length ? `<category>${item.data.tags.join(',')}</category>` : ''}
+          ${item.data.draft ? '<status>Draft</status>' : ''}
+        `
       }))
     )
-  })
-}
-
-export { GET }
+  });
+};
